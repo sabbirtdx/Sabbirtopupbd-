ce1ba3211b35519a0faae5d5d875b6e59305519e
sabbirtdx <sabbirtdx9@gmail.com>
2025-06-18 22:31:50 +0600

    Update app.js

--- js/js/app.js
+++ js/js/app.js
@@ -1,1 +1,13 @@
+function showSection(id) {
+  document.getElementById('balance').style.display = 'none';
+  document.getElementById('topup').style.display = 'none';
+  document.getElementById(id).style.display = 'block';
+}
 
+function addBalance() {
+  alert("Balance add request sent!");
+}
+
+function submitTopup() {
+  alert("Topup request sent!");
+}
